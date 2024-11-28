fc68706d4f91e4d4eaf04c8c6dfb636557272eba
alejandro202113019 <alejandro.mesa@uptc.edu.co>
2024-11-28 18:05:33 -0500

    Primer commit

---
+++ src/main/java/co/edu/uptc/ParcialSpringAlejandroMesa/ParcialSpringAlejandroMesaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.ParcialSpringAlejandroMesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParcialSpringAlejandroMesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParcialSpringAlejandroMesaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/uptc/ParcialSpringAlejandroMesa/ParcialSpringAlejandroMesaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uptc.ParcialSpringAlejandroMesa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSpringAlejandroMesaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
